0a69430e0ff8d0be2c3d4f05ae14d5cd750f0ea8
Umesh Joshi <uj19343@gmail.com>
2025-06-29 17:42:09 +0545

    multer change

--- build/routes/product.routes.js
+++ build/routes/product.routes.js
@@ -13,7 +13,7 @@ const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./uploads");
     },
-    //it can create teh qnuique file name using the date math and file name
+    //it can create the uique file name using the date math and file name
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
         cb(null, uniqueSuffix + file.originalname);
